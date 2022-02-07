52ac9402632659e6b7ff5c0828954f2c3d3dabf7
jguibrandao <jguibrandao@gmail.com>
2022-02-07 00:21:38 -0300

    Adicionando mais rotas e suas instruções

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,12 @@ routes.get("/products/:user_id") // Mostra produtos de usuário
 routes.patch("products/:user_id/:product_id") // Atualiza produto de usuário
 routes.delete("/products/user_id/:product_id") // Deleta determinado produto de determinado usuário
 
+routes.get("/products") // Lista todos os produtos
 routes.get("/products/:product_id") // Mostra determinado produto
+
+routes.post("/cart/:user_id") // Cria carrinho para determinado usuário
+routes.get("/carts/:user_id") // Mostra todos os carrinhos de um cliente
+
+routes.get("/cart/:user_id/:cart_id") // Mostra carrinho específico de determinado cliente
 
 module.exports = routes
