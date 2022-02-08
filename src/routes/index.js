3dd2e93d6a4b0c711d1823ca05a35af5aebf62cc
jguibrandao <jguibrandao@gmail.com>
2022-02-08 02:07:40 -0300

    Mudando caminhos de importação, por ter mudado o nome; e adicionando a execução dos métodos as rotas de produtos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require("express")
 
-const UserController = require("../controllers/userController")
+const UserController = require("../controllers/UserController")
+const SessionController = require("../controllers/Session")
+const ProductController = require("../controllers/ProductController")
 
 const routes = Router()
 
@@ -9,19 +11,19 @@ routes.get("/", (req, res) => {
 })
 
 routes.post("/users", UserController.createUser) // Cria usuários
-routes.get("/users") // Mostra usuários
+routes.get("/users", UserController.getUsers) // Mostra usuários
 
-routes.get("users/:user_id") // Mostra usuário específico
+routes.get("/users/:user_id", UserController.getUserById) // Mostra usuário específico
 
-routes.post("/login") // Cria/faz login
+routes.post("/sessions", SessionController.createSession) // Cria/faz login
 
-routes.post("/products/:user_id") // Cria produtos para usuário
-routes.get("/products/:user_id") // Mostra produtos de usuário
-routes.patch("products/:user_id/:product_id") // Atualiza produto de usuário
-routes.delete("/products/user_id/:product_id") // Deleta determinado produto de determinado usuário
+routes.post("/products/:user_id", ProductController.createProduct) // Cria produtos para usuário
+routes.get("/products/:user_id", ProductController.getUserProducts) // Mostra produtos de usuário
+routes.patch("/products/:user_id/:product_id", ProductController.updateProduct) // Atualiza produto de usuário
+routes.delete("/products/user_id/:product_id", ProductController.deleteProduct) // Deleta determinado produto de determinado usuário
 
-routes.get("/products") // Lista todos os produtos
-routes.get("/products/:product_id") // Mostra determinado produto
+routes.get("/products", ProductController.getAllProducts) // Lista todos os produtos
+routes.get("/products/:product_id", ProductController.getProductById) // Mostra determinado produto
 
 routes.post("/cart/:user_id") // Cria carrinho para determinado usuário
 routes.get("/carts/:user_id") // Mostra todos os carrinhos de um cliente
