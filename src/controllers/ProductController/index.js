6ac8b906501a196d568d4e98fe2bd960e25ff9a5
jguibrandao <jguibrandao@gmail.com>
2022-02-08 19:54:29 -0300

    Adicionando e testando conteúdo dos métodos getUserProducts e updateProduct

--- src/controllers/ProductController/index.js
+++ src/controllers/ProductController/index.js
@@ -10,23 +10,30 @@ const ProductController = {
 
             const newProduct = await Product.create(data)
             await newProduct.populate('username')
-            return res.status(200).send(newProduct)
+            return res.status(200).json(newProduct)
         } catch (err) {
             return res.status(400).json(err)
         }
     },
 
     async getUserProducts(req, res) {
+        const { user_id } = req.params
+
         try {
-            
+            const productsOfAnUser = await Product.find({ username: user_id }) 
+            return res.status(200).json(productsOfAnUser)
         } catch (err) {
             return res.status(400).json(err)
         }
     },
 
     async updateProduct(req, res) {
+        const bodyData = req.body
+        const { product_id, user_id } = req.params
+
         try {
-            
+            const updateProduct = await Product.findByIdAndUpdate(product_id, bodyData, { new: true })
+            return res.status(200).json(updateProduct)
         } catch (err) {
             return res.status(400).json(err)
         }
