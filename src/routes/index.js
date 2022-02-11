c2715352808c0a1de7e8437a12cf6bf5269300ac
jguibrandao <jguibrandao@gmail.com>
2022-02-11 10:41:35 -0300

    importando e instanciando o middleware authenticate

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ const SessionController = require("../controllers/Session")
 const ProductController = require("../controllers/ProductController")
 const CartController = require("../controllers/CartController")
 
+const { authenticate } = require("../middlewares/index")
+
 const routes = Router()
 
 routes.get("/", (req, res) => {
@@ -18,17 +20,17 @@ routes.get("/users/:user_id", UserController.getUserById) // Mostra usuário esp
 
 routes.post("/sessions", SessionController.createSession) // Cria/faz login
 
-routes.post("/products/:user_id", ProductController.createProduct) // Cria produtos para usuário
+routes.post("/products/:user_id", authenticate, ProductController.createProduct) // Cria produtos para usuário
 routes.get("/:user_id/products", ProductController.getUserProducts) // Mostra produtos de usuário
-routes.patch("/products/:user_id/:product_id", ProductController.updateProduct) // Atualiza produto de usuário
-routes.delete("/products/user_id/:product_id", ProductController.deleteProduct) // Deleta determinado produto de determinado usuário
+routes.patch("/products/:user_id/:product_id", authenticate, ProductController.updateProduct) // Atualiza produto de usuário
+routes.delete("/products/:user_id/:product_id", authenticate, ProductController.deleteProduct) // Deleta determinado produto de determinado usuário
 
 routes.get("/products", ProductController.getAllProducts) // Lista todos os produtos
 routes.get("/products/:product_id", ProductController.getProductById) // Mostra determinado produto
 
-routes.post("/cart/:user_id", CartController.createCart) // Cria carrinho para determinado usuário
-routes.get("/:user_id/carts", CartController.getUserCarts) // Mostra todos os carrinhos de um cliente
-
-routes.get("/cart/:user_id/:cart_id", CartController.getCart) // Mostra carrinho específico de determinado cliente
+routes.post("/cart/:user_id", authenticate, CartController.createCart) // Cria carrinho para determinado usuário
+routes.get("/carts/:user_id", authenticate, CartController.getUserCarts) // Mostra todos os carrinhos de um cliente
+routes.delete("/carts/:user_id/:cart_id", authenticate, CartController.deleteCart)
+routes.get("/cart/:user_id/:cart_id", authenticate, CartController.getCart) // Mostra carrinho específico de determinado cliente
 
 module.exports = routes
