a6f7f99734046490ce4dec8bd393795ddf27f09a
jguibrandao <jguibrandao@gmail.com>
2022-02-11 10:40:14 -0300

    Consertando o método getUserCarts, adicionando o método deleteCart, e limpando o código

--- src/controllers/CartController/index.js
+++ src/controllers/CartController/index.js
@@ -18,15 +18,29 @@ const CartController = {
 
         try {
             const userCarts = await Cart.find({ username: user_id })
-            return res.status(200),json(userCarts)
+            return res.status(200).json(userCarts)
+        } catch (err) {
+            return res.status(400).json(err)
+        }
+    },
+
+    async deleteCart(req, res) {
+        const { cart_id } = req.params
+
+        try {
+            const deletedCart = await Cart.findByIdAndDelete(cart_id)
+            return res.status(200).json(deletedCart)
         } catch (err) {
             return res.status(400).json(err)
         }
     },
 
     async getCart(req, res) {
+        const { cart_id } = req.params
+
         try {
-            
+            const cart = await Cart.findById(cart_id)
+            return res.status(200).json(cart)
         } catch (err) {
             return res.status(400).json(err)
         }
